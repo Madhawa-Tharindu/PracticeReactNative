be59837c846dd706653281d95f646dd9e51aaa78
Madhawa-Tharindu <madhawath@gmail.com>
2023-11-03 12:59:19 +0530

    created buttons, view & text

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Linking } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text style={styles.text}>Hi Lakindu</Text>
+      {/* <Button title="Click Me" onPress={() => alert('Button Clicked')}></Button> */}
+      <Button title="Click Me" onPress={() => {Linking.openURL('https://www.youtube.com/watch?v=ANdSdIlgsEw&list=PLGaOwEr4JYjdwgmFVqU6ZMeNrNNR-zTtm&index=3')}}></Button>
     </View>
   );
 }
@@ -13,8 +15,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#abf7b1',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: 'black',
+    fontSize: 20,
+    fontWeight: 'bold'
+  }
 });
