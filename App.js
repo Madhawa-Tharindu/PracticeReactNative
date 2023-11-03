e45600060c835d8dd3a8b3775a06eeb3e3653031
Madhawa-Tharindu <madhawath@gmail.com>
2023-11-03 13:22:21 +0530

    used useState

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
+import react, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Linking } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  Button, 
+  Linking 
+} from 'react-native';
 
 export default function App() {
+
+const [name, setName] = useState('Lakindu');
+const [session, setSession] = useState({number: 6, title: 'state'});
+
+const onClickHandler = () => {
+  setName('Lakindu');
+  setSession({number: 7, title: 'moved to next session'});
+}
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Text style={styles.text}>Hi Lakindu</Text>
+      <Text style={styles.text}>I'm {name}</Text>
+      <Text style={styles.text}>this is session number  {session.number} and about {session.title}</Text>
       {/* <Button title="Click Me" onPress={() => alert('Button Clicked')}></Button> */}
-      <Button title="Click Me" onPress={() => {Linking.openURL('https://www.youtube.com/watch?v=ANdSdIlgsEw&list=PLGaOwEr4JYjdwgmFVqU6ZMeNrNNR-zTtm&index=3')}}></Button>
+      {/* <Button title="Click Me" onPress={() => {Linking.openURL('https://www.youtube.com/watch?v=ANdSdIlgsEw&list=PLGaOwEr4JYjdwgmFVqU6ZMeNrNNR-zTtm&index=3')}}></Button> */}
+      <Button title="Update The Name" onPress={onClickHandler}></Button>
     </View>
   );
 }
